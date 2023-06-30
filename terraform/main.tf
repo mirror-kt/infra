b50e3d005bda9312af859463514481641194a8e0
mirror-kt <me@mirror-kt.dev>
2023-06-30 21:59:57 +0900

    🎉 Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+  }
+
+  cloud {
+    organization = "mirror_kt"
+
+    workspaces {
+      name = "mirror_kt"
+    }
+  }
+}
